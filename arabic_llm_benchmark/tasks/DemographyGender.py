53bf48582667a68b2d84499bc8d4fc3b14b5c333
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2023-05-29 07:55:23 +0300

    DemographyGenderTask: Implement handling of failed requests

--- arabic_llm_benchmark/tasks/DemographyGender.py
+++ arabic_llm_benchmark/tasks/DemographyGender.py
@@ -4,6 +4,7 @@ from sklearn.metrics import f1_score
 
 class DemographyGenderTask(TaskBase):
 	def evaluate(self, true_labels, predicted_labels):
+		predicted_labels = [p if p else "Failed" for p in predicted_labels]
 		return {
 			"Macro F1": f1_score(true_labels, predicted_labels, average='macro')
 		}
