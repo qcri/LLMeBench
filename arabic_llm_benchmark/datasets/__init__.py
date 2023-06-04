3b2e257de4691d3866f6e6de2a74cf2f20ef27cb
Ahmed Abdelali <ahmed.abdelali@gmail.com>
2023-06-04 15:08:09 +0300

    Add AraBench Dataset and MachineTranslation task (#8)
    Add AraBench dataset, MachineTranslation task and an MT asset to the benchmark.
    
    Detailed commit history:
    
    * add Arabic to English MT dataset
    
    * Update code, rename classes for AraBench
    
    * format code
    
    * add AraBench files
    
    * add assets AraBench_Ara2Eng_ChatGPT_ZeroShot.py
    
    * Update MT task name
    
    * format code
    
    * fix MT module task
    
    * Add ArabicSequenceTagging
    
    * Revert "Add ArabicSequenceTagging"
    
    This reverts commit a9f5ca6afb0a9caba7a634a39dda12ab27a62cb2.
    
    * Add get_data_sample to AraBench
    
    * Fix machine translation evaluation
    
    ---------
    
    Co-authored-by: Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>

--- arabic_llm_benchmark/datasets/__init__.py
+++ arabic_llm_benchmark/datasets/__init__.py
@@ -1,1 +1,2 @@
+from .AraBench import AraBenchDataset
 from .ArabGend import ArabGendDataset
--- arabic_llm_benchmark/tasks/__init__.py
+++ arabic_llm_benchmark/tasks/__init__.py
@@ -1,1 +1,2 @@
 from .DemographyGender import DemographyGenderTask
+from .MachineTranslation import MachineTranslationTask
