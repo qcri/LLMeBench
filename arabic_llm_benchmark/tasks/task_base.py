c30531244b24c5a448b15126abdcbcbec4e86535
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2023-06-04 11:09:36 +0300

    Add task method to get random predictions
    One way to deal with failed predictions is to randomly select one,
    this commit adds support for this in the `TaskBase` class.

--- arabic_llm_benchmark/tasks/task_base.py
+++ arabic_llm_benchmark/tasks/task_base.py
@@ -1,3 +1,4 @@
+import random
 from abc import ABC, abstractmethod
 
 
@@ -7,6 +8,9 @@ class TaskBase(ABC):
 
     def load_data(self, data_path):
         return self.dataset.load_data(data_path)
+
+    def get_random_prediction(self, label_set):
+        return random.choice(list(label_set))
 
     @abstractmethod
     def evaluate(self, true_labels, predicted_labels):
