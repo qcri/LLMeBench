69662902376fe60f20da177d32dce55efd74ea63
Firoj Alam <firojalam@gmail.com>
2023-06-06 15:07:06 +0300

    tasks for firoj branch updated

--- arabic_llm_benchmark/datasets/__init__.py
+++ arabic_llm_benchmark/datasets/__init__.py
@@ -1,10 +1,9 @@
 from .AraBench import AraBenchDataset
 from .ArabGend import ArabGendDataset
+from .ArabicSegmentation import ArabicSegmentationDataset
 from .Attentionworthy import AttentionworthyDataset
 from .Checkworthiness import CheckworthinessDataset
 from .FactualityKhouja20 import FactualityKhouja20Dataset
 from .NewsCatAlKhaleej import NewsCatAlKhaleejDataset
 from .StanceKhouja20 import StanceKhouja20Dataset
 from .Subjectivity import SubjectivityDataset
-from .ArabicSegmentation import ArabicSegmentationDataset
-
