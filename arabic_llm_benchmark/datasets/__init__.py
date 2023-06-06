9832ef0f7d164c4991d0ff03b704da72b20c15f3
Ahmed Abdelali <ahmed.abdelali@gmail.com>
2023-06-06 14:16:39 +0300

    Add ArabicSegmentation Task and Dataset (#17)
    * Add ArabicSequenceTagging
    
    * push Segmentation task
    
    * Add segmentation changes
    
    * push segmentation and merge changes
    
    * Fix evaluation
    
    * change None data to unsegmented words
    
    * fix none segmentation, re-format code

--- arabic_llm_benchmark/datasets/__init__.py
+++ arabic_llm_benchmark/datasets/__init__.py
@@ -1,4 +1,5 @@
 from .AraBench import AraBenchDataset
 from .ArabGend import ArabGendDataset
+from .ArabicSegmentation import ArabicSegmentationDataset
 from .Khouja20Claim import Khouja20ClaimDataset
 from .Khouja20Stance import Khouja20StanceDataset
