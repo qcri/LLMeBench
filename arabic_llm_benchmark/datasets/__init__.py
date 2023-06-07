5e3ccfd1b7549c13f65d1ee7b6006162185b5052
Firoj Alam <firojalam@gmail.com>
2023-06-07 14:07:39 +0300

    fixed init files

--- arabic_llm_benchmark/datasets/__init__.py
+++ arabic_llm_benchmark/datasets/__init__.py
@@ -6,6 +6,8 @@ from .Checkworthiness import CheckworthinessDataset
 from .Claim import CovidClaimDataset
 from .FactualityKhouja20 import FactualityKhouja20Dataset
 from .Harmful import CovidHarmfulDataset
+from .NewsCatAkhbarona import NewsCatAkhbaronaDataset
+from .NewsCatAlArabiya import NewsCatAlArabiyaDataset
 from .NewsCatAlKhaleej import NewsCatAlKhaleejDataset
 from .Propaganda import PropagandaTweetDataset
 from .StanceKhouja20 import StanceKhouja20Dataset
