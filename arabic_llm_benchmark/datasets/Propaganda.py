7b395cbe841334d69723bd7220247cdfcd86ed58
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2023-06-07 10:43:34 +0300

    Add default techniques to PropogandaDataset

--- arabic_llm_benchmark/datasets/Propaganda.py
+++ arabic_llm_benchmark/datasets/Propaganda.py
@@ -1,12 +1,14 @@
 import json
 
+from pathlib import Path
+
 from arabic_llm_benchmark.datasets.dataset_base import DatasetBase
 
 
 class PropagandaTweetDataset(DatasetBase):
-    def __init__(self, **kwargs):
+    def __init__(self, techniques_path=None, **kwargs):
         # Get the path to the file listing the target techniques
-        self.techniques_fpath = kwargs["techniques_path"]
+        self.techniques_path = Path(techniques_path) if techniques_path else None
         super(PropagandaTweetDataset, self).__init__(**kwargs)
 
     def citation(self):
@@ -18,9 +20,32 @@ class PropagandaTweetDataset(DatasetBase):
         return {"input": "Tweet", "label": ["no technique"]}
 
     def get_predefined_techniques(self):
-        # Load a pre-defined list of propaganda techniques
-        with open(self.techniques_fpath, "r", encoding="utf-8") as f:
-            techniques = [label.strip() for label in f.readlines()]
+        # Load a pre-defined list of propaganda techniques, if available
+        if self.techniques_path and self.techniques_path.exists():
+            with open(self.techniques_path, "r", encoding="utf-8") as f:
+                techniques = [label.strip() for label in f.readlines()]
+        else:
+            techniques = [
+                "no technique",
+                "Smears",
+                "Exaggeration/Minimisation",
+                "Loaded Language",
+                "Appeal to fear/prejudice",
+                "Name calling/Labeling",
+                "Slogans",
+                "Repetition",
+                "Doubt",
+                "Obfuscation, Intentional vagueness, Confusion",
+                "Flag-waving",
+                "Glittering generalities (Virtue)",
+                "Misrepresentation of Someone's Position (Straw Man)",
+                "Presenting Irrelevant Data (Red Herring)",
+                "Appeal to authority",
+                "Whataboutism",
+                "Black-and-white Fallacy/Dictatorship",
+                "Thought-terminating cliché",
+                "Causal Oversimplification",
+            ]
 
         return techniques
 
