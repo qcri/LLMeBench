55b4322fb00258a646d7d5fa5c78c741c0171cf9
Basel Mousi <bmousi@hbku.edu.qa>
2023-06-11 13:32:34 +0300

    modified imports in init file

--- arabic_llm_benchmark/tasks/__init__.py
+++ arabic_llm_benchmark/tasks/__init__.py
@@ -2,4 +2,4 @@ from .DemographyGender import DemographyGenderTask
 from .FactClaim import FactClaimTask
 from .FactStance import FactStanceTask
 from .MachineTranslation import MachineTranslationTask
-from .Ner import NERTask
+from .NER import NERTask
