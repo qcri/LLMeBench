20ca290316ce7eb69a3f216ea97c74f51ba2cc80
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2023-06-13 11:55:00 +0300

    Move fewshot methods to base class

--- arabic_llm_benchmark/datasets/Claim.py
+++ arabic_llm_benchmark/datasets/Claim.py
@@ -1,9 +1,6 @@
 import pandas as pd
 
 from arabic_llm_benchmark.datasets.dataset_base import DatasetBase
-from langchain.prompts.example_selector import MaxMarginalRelevanceExampleSelector
-from langchain.vectorstores import FAISS
-from langchain.embeddings import HuggingFaceEmbeddings
 
 class CovidClaimDataset(DatasetBase):
     def __init__(self, **kwargs):
@@ -35,60 +32,3 @@ class CovidClaimDataset(DatasetBase):
                 formatted_data.append({"input": text, "label": label, "line_number": index, "input_id": twt_id})
 
         return formatted_data
-
-    def load_train_data(self, train_data_path):
-        formatted_data = []
-
-        with open(train_data_path, "r", encoding='utf-8') as in_file:
-            next(in_file)
-            for index, line in enumerate(in_file):
-                tweet = [str(s.strip()) for s in line.split("\t")]
-                text = tweet[3]
-                label = tweet[4]
-                twt_id = tweet[1]
-
-                # For later on in FS, langchain expect all values to be strings
-                formatted_data.append({"input": text, "label": label, "line_number": str(index), "input_id": twt_id})
-
-        return formatted_data
-
-
-    def dedup_train_test(self, target_data, train_data):
-        final_train_data = []
-        test_ids = [tid['input_id'] for tid in target_data]
-
-        for train_sample in train_data:
-            if train_sample['input_id'] in test_ids: continue
-            final_train_data.append(train_sample)
-
-        return final_train_data
-
-    # MARAM: this function should be called once as we do when we load train data
-    # MARAM: n_shots should be a parameter passed form the asset
-    def prepare_fewshots(self, target_data, train_data, n_shots):
-        few_shots_per_input = {}
-
-        print(len(train_data))
-        # Dedup train set against test set by doc ID before selecting examples
-        # We discovered some datasets had overlap between train and test
-        train_data = self.dedup_train_test(target_data, train_data)
-        print(len(train_data))
-
-        # MARAM: MaxMarginalRelevanceExampleSelector should be a variable
-        # Cache is used to store the bert model we are using
-        example_selector = MaxMarginalRelevanceExampleSelector.from_examples(train_data, HuggingFaceEmbeddings(
-            model_name="distiluse-base-multilingual-cased-v1",
-            cache_folder="assets/sentence-transformers_distiluse-base-multilingual-cased-v1/"),
-                                                                             FAISS, k=n_shots)
-
-        # TODO: Should be an iterator
-        # For each input sample, get few shot examples
-        for input_sample in target_data:
-            input_sample_content = input_sample['input']
-            # Only need the input test content to select examples
-            examples = example_selector.select_examples({"input": input_sample_content, "label": ""})
-
-            # Quick way to pre-compute examples for all test data at once.
-            few_shots_per_input[input_sample_content] = examples
-
-        return few_shots_per_input
--- arabic_llm_benchmark/datasets/dataset_base.py
+++ arabic_llm_benchmark/datasets/dataset_base.py
@@ -1,5 +1,9 @@
 from abc import ABC, abstractmethod
 
+from langchain.prompts.example_selector import MaxMarginalRelevanceExampleSelector
+from langchain.vectorstores import FAISS
+from langchain.embeddings import HuggingFaceEmbeddings
+
 class DatasetBase(ABC):
     def __init__(self, **kwargs):
         pass
@@ -23,26 +27,48 @@ class DatasetBase(ABC):
         which will be saved in the cache
         """
         pass
-    @abstractmethod
-    def load_train_data(self, train_data_path):
-        """
-        Returns a list of dictionaries,
-        with at least the following keys:
-                "input": <input-instance>
-                "label": <label>
-        The dictionaries can contain other keys as well
-        which will be saved in the cache
-        """
-        pass
 
-    @abstractmethod
-    def prepare_fewshots(self, target_data, train_data, n_shots):
-        """
-        Returns a list of dictionaries,
-        with at least the following keys:
-                "input": <input-instance>
-                "label": <label>
-        The dictionaries can contain other keys as well
-        which will be saved in the cache
-        """
-        pass
+    def deduplicate_train_test(self, train_data, test_data):
+        if len(test_data) > 0 and "input_id" not in test_data[0]:
+            logging.warning("`input_id` not found in data, no deduplication will be run")
+            # TODO: Add fallback to input, label deep comparison
+            return train_data
+
+        final_train_data = []
+        test_ids = [tid['input_id'] for tid in test_data]
+
+        for train_sample in train_data:
+            if train_sample['input_id'] in test_ids: continue
+            final_train_data.append(train_sample)
+
+
+        return final_train_data
+
+    def prepare_fewshots(self, target_data, train_data, n_shots, deduplicate=True):
+        """Prepares fewshot samples _per test instance_"""
+        few_shots_per_input = {}
+
+        # Dedup train set against test set by doc ID before selecting examples
+        # We discovered some datasets had overlap between train and test
+        if deduplicate:
+            train_data = self.deduplicate_train_test(target_data, train_data)
+
+        # TODO: MaxMarginalRelevanceExampleSelector should be generalized
+        # TODO: Need to handle not str inputs
+        embedding_model = HuggingFaceEmbeddings(model_name="distiluse-base-multilingual-cased-v1")
+        example_selector = MaxMarginalRelevanceExampleSelector.from_examples(train_data, embedding_model, 
+                                                                             FAISS, input_keys=["input"], k=n_shots)
+
+        # TODO: Convert to an iterator
+        # For each input sample, get few shot examples
+        for input_sample in target_data:
+            input_sample_content = input_sample['input']
+
+            # Only need the input test content to select examples
+            examples = example_selector.select_examples({"input": input_sample_content, "label": ""})
+
+            # Quick way to pre-compute examples for all test data at once.
+            few_shots_per_input[input_sample_content] = examples
+
+        return few_shots_per_input
+
