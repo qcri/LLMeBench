0ee3881c6592c4515945a020b633ce484789e7e7
Basel Mousi <bmousi@hbku.edu.qa>
2023-06-13 20:30:08 +0300

    added asset for QA data

---
+++ assets/benchmark_v1/QA/AraQA_ChatGPT_ZeroShot.py
@@ -0,0 +1,41 @@
+import os
+
+from arabic_llm_benchmark.datasets import AraQADataset
+from arabic_llm_benchmark.models import GPTModel, RandomGPTModel
+from arabic_llm_benchmark.tasks import QATask
+
+
+def config():
+    return {
+        "dataset": AraQADataset,
+        "dataset_args": {},
+        "task": QATask,
+        "task_args": {},
+        "model": GPTModel,
+        "model_args": {
+            "api_type": "azure",
+            "api_version": "2023-03-15-preview",
+            "api_base": os.environ["AZURE_API_URL"],
+            "api_key": os.environ["AZURE_API_KEY"],
+            "engine_name": "gpt",
+            "class_labels": "NA",
+            "max_tries": 3,
+        },
+        "general_args": {"data_path": "data/QA"},
+    }
+
+
+def prompt(input_sample):
+    return {
+        "system_message": "Assistant is a large language model trained by OpenAI.",
+        "messages": [
+            {
+                "sender": "user",
+                "text": f"Your task is to answer questions in Arabic based on a given context.\nNote: Your answers should be spans extracted from the given context without any illustrations.\nYou don't need to provide a complete answer\nContext:{input_sample['context']}\nQuestion:{input_sample['question']}\nAnswer:",
+            }
+        ],
+    }
+
+
+def post_process(response):
+    return response["choices"][0]["text"]
