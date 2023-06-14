43a5d9b199e8c3e3cc9989a8d28c597f11bf60df
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2023-06-14 07:31:57 +0300

    Fix ChatCompletion prompt in ArSASSentiment

--- assets/benchmark_v1/sentiment/sentiment/ArSASSentiment_GPTChatCompletion_ZeroShot.py
+++ assets/benchmark_v1/sentiment/sentiment/ArSASSentiment_GPTChatCompletion_ZeroShot.py
@@ -28,15 +28,16 @@ def config():
 
 
 def prompt(input_sample):
-    return {
-        "system_message": "You are an AI assistant that helps people find information.",
-        "messages": [
-            {
-                "sender": "user",
-                "text": f"Choose only one sentiment between: Positive, Negative, Neutral, or Mixed for this Arabic sentence: \n {input_sample}",
-            }
-        ],
-    }
+    return [
+        {
+            "role": "system",
+            "content": "You are an AI assistant that helps people find information.",
+        },
+        {
+            "role": "user",
+            "content": f"Choose only one sentiment between: Positive, Negative, Neutral, or Mixed for this Arabic sentence: \n {input_sample}",
+        },
+    ]
 
 
 def post_process(response):
