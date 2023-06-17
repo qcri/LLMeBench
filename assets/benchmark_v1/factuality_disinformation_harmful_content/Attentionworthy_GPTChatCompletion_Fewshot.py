d5e925be7ae858286c074868f9c15e581bc46154
Firoj Alam <firojalam@gmail.com>
2023-06-17 22:37:08 +0300

    updated prompt and postprocessing

--- assets/benchmark_v1/factuality_disinformation_harmful_content/Attentionworthy_GPTChatCompletion_Fewshot.py
+++ assets/benchmark_v1/factuality_disinformation_harmful_content/Attentionworthy_GPTChatCompletion_Fewshot.py
@@ -48,10 +48,13 @@ def config():
 def few_shot_prompt(input_sample, base_prompt, examples):
     out_prompt = base_prompt + "\n"
     for example in examples:
-        # Found chatgpt confused when using 0 and 1 in the prompt
-        label = "no" if example["label"] == "0" else "yes"
         out_prompt = (
-            out_prompt + "tweet: " + example["input"] + "\nlabel: " + label + "\n\n"
+            out_prompt
+            + "tweet: "
+            + example["input"]
+            + "\nlabel: "
+            + example["label"]
+            + "\n\n"
         )
 
     # Append the sentence we want the model to predict for but leave the Label blank
@@ -61,7 +64,7 @@ def few_shot_prompt(input_sample, base_prompt, examples):
 
 
 def prompt(input_sample, examples):
-    base_prompt = f'Annotate the following "tweet" into one of the following categories: yes_discusses_action_taken, harmful, yes_discusses_cure, yes_asks_question, no_not_interesting, yes_other, yes_blame_authorities, yes_contains_advice, yes_calls_for_action\n\n'
+    base_prompt = f'Annotate the following "tweet" into one of the following categories: yes_discusses_action_taken, harmful, yes_discusses_cure, yes_asks_question, no_not_interesting, yes_other, yes_blame_authorities, yes_contains_advice, yes_calls_for_action. Provide only label.\n\n'
     return [
         {
             "role": "system",
@@ -76,7 +79,7 @@ def prompt(input_sample, examples):
 
 def post_process(response):
     label = response["choices"][0]["message"]["content"]
-
+    label_list = config()["model_args"]["class_labels"]
     label = (
         label.lower()
         .replace(" - ", ", ")
@@ -94,5 +97,29 @@ def post_process(response):
         label_fixed = "harmful"
     elif label.startswith("yes"):
         label_fixed = label
+    elif "label: yes_blame_authoritie" in label:
+        label_fixed = "yes_blame_authoritie"
+    elif "label: yes_discusses_action_taken" in label:
+        label_fixed = "yes_discusses_action_taken"
+    elif "label: harmful" in label:
+        label_fixed = "harmful"
+    elif "label: yes_discusses_cure" in label:
+        label_fixed = "yes_discusses_cure"
+    elif "label: yes_asks_question" in label:
+        label_fixed = "yes_asks_question"
+    elif "label: no_not_interesting" in label:
+        label_fixed = "no_not_interesting"
+    elif "label: yes_other" in label:
+        label_fixed = "yes_other"
+    elif "label: yes_blame_authorities" in label:
+        label_fixed = "yes_blame_authorities"
+    elif "label: yes_contains_advice" in label:
+        label_fixed = "yes_contains_advice"
+    elif "label: yes_calls_for_action" in label:
+        label_fixed = "yes_calls_for_action"
+    elif label in label_list:
+        label_fixed = label
+    else:
+        label_fixed = None
 
     return label_fixed
