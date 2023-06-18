b3576b971675c348cfe995e5b0bceaa222b7dc5a
Ahmed Abdelali <ahmed.abdelali@gmail.com>
2023-06-18 16:40:51 +0300

    Add Parsing dataset/task/assets

---
+++ assets/benchmark_v1/sequence_tagging_ner_pos_etc/parsing_ChatGPT4_ZeroShot.py
@@ -0,0 +1,45 @@
+import os
+
+from arabic_llm_benchmark.datasets import ArabicParsingDataset
+from arabic_llm_benchmark.models import GPTChatCompletionModel
+from arabic_llm_benchmark.tasks import ArabicParsingTask
+
+
+def config():
+    return {
+        "dataset": ArabicParsingDataset,
+        "dataset_args": {},
+        "task": ArabicParsingTask,
+        "task_args": {},
+        "model": GPTChatCompletionModel,
+        "model_args": {
+            "api_type": "azure",
+            "api_version": "2023-03-15-preview",
+            "api_base": os.environ["AZURE_API_URL"],
+            "api_key": os.environ["AZURE_API_KEY"],
+            "engine_name": os.environ["ENGINE_NAME"],
+            # "class_labels": ["m", "f"],
+            "max_tries": 3,
+        },
+        "general_args": {
+            "data_path": "data/sequence_tagging_ner_pos_etc/Parsing/arabic_PADT_test_gs.conll"
+        },
+    }
+
+
+def prompt(input_sample):
+    return [
+        {
+            "role": "system",
+            "content": "You are a linguist that helps in annotating data.",           
+        },
+        {
+            "role": "user",
+            "content":  f"Given the following features (in order: ID, Form, Lemma, CPostTag, POSTag, Features), predict the Head of each token in the following sentence, which is either a value of a related ID or 0. A value of zero means the token attaches to the virtual root node: {input_sample}"
+        }
+    ]
+
+
+def post_process(response):
+    output = response['choices'][0]['message']['content'].strip().split('\n')
+    return output
