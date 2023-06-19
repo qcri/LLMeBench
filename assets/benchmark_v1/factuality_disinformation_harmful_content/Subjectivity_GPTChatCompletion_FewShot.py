24d79d2f92014a5f6a0a4a0331e4ed8d1689feb0
Sabri Boughorbel <bsabri@yahoo.com>
2023-06-19 15:08:49 +0300

    Update fewshot asset for Subjectivity (#94)
    * import logging missing for few shots
    
    * Subjectivity asset added
    
    * Remove old Subjectivity fewshot asset and replace with current one
    
    ---------
    
    Co-authored-by: Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>

--- assets/benchmark_v1/factuality_disinformation_harmful_content/Subjectivity_GPTChatCompletion_FewShot.py
+++ assets/benchmark_v1/factuality_disinformation_harmful_content/Subjectivity_GPTChatCompletion_FewShot.py
@@ -1,13 +1,8 @@
 import os
-import random
-import re
 
 from arabic_llm_benchmark.datasets import SubjectivityDataset
 from arabic_llm_benchmark.models import GPTChatCompletionModel
 from arabic_llm_benchmark.tasks import SubjectivityTask
-
-
-random.seed(1333)
 
 
 def config():
@@ -29,59 +24,63 @@ def config():
         "general_args": {
             "data_path": "data/factuality_disinformation_harmful_content/subjectivity/dev_ar.tsv",
             "fewshot": {
-                "train_data_path": "data/factuality_disinformation_harmful_content/subjectivity/train_ar.tsv",
+                "train_data_path": "data/factuality_disinformation_harmful_content/subjectivity/train_ar.tsv"
             },
         },
     }
 
 
-def few_shot_prompt(input_sample, base_prompt, examples):
-    out_prompt = base_prompt + "\n\n"
-    for example in examples:
-        out_prompt = (
-            out_prompt
-            + "tweet: "
-            + example["input"]
-            + "\nlabel: "
-            + example["label"]
-            + "\n\n"
-        )
-
-    out_prompt = out_prompt + "tweet: " + input_sample + "\nlabel: \n"
-
-    return out_prompt
-
-
 def prompt(input_sample, examples):
-    base_prompt = f'Annotate the "sentence" as subjective or objective'
+    base_prompt = (
+        "Classify the sentence as Subjective or Objective. Provide only label.\n"
+    )
     return [
         {
             "role": "system",
-            "content": "You are an expert annotator, who can analyze the information in the sentence and determine whether the sentence is subjective or objective.",
+            # "content": "You are social media expert. You can annotate important tweets and require attention from journalists, fact-checker, and government entities.",
+            "content": "You are an AI assistant that helps people find information.",
         },
         {
             "role": "user",
             "content": few_shot_prompt(input_sample, base_prompt, examples),
         },
     ]
+    # return {
+    #     "system_message": "You are an AI assistant that helps people find information.",
+    #     "messages": [
+    #         {
+    #             "sender": "user",
+    #             "text": few_shot_prompt(input_sample, base_prompt, examples),
+    #         }
+    #     ],
+    # }
+
+
+def few_shot_prompt(input_sample, base_prompt, examples):
+    out_prompt = base_prompt + "\n"
+    for example in examples:
+        # Found chatgpt confused when using 0 and 1 in the prompt
+        label = "objective" if example["label"] == "OBJ" else "subjective"
+        out_prompt = (
+            out_prompt + "Sentence: " + example["input"] + "\nLabel: " + label + "\n\n"
+        )
+
+    # Append the sentence we want the model to predict for but leave the Label blank
+    out_prompt = out_prompt + "Sentence: " + input_sample + "\nLabel: \n"
+
+    # print("=========== FS Prompt =============\n")
+    # print(out_prompt)
+
+    return out_prompt
 
 
 def post_process(response):
     label = response["choices"][0]["message"]["content"].lower()
 
-    if "label: objective" in label:
+    if label == "objective" or label == "objective.":
         label_fixed = "OBJ"
-    elif "label: subjective" in label:
-        label_fixed = "SUBJ"
-    elif label == "objective" or label == "objective.":
-        label_fixed = "OBJ"
+
     elif label == "subjective" or label == "subjective.":
         label_fixed = "SUBJ"
-    elif "label: obj" in label or label == "obj" or label == "obj.":
-        label_fixed = "OBJ"
-    elif "label: subj" in label or label == "subj" or label == "subj.":
-        label_fixed = "SUBJ"
-    else:
-        label_fixed = None
 
     return label_fixed
