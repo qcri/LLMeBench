c2ea00bf392042acce528242f737b7780e86ccc9
Ahmed Abdelali <ahmed.abdelali@gmail.com>
2023-06-19 15:55:47 +0300

    Update ArabicPOS_v4.py
    clean code

--- arabic_llm_benchmark/tasks/ArabicPOS_v4.py
+++ arabic_llm_benchmark/tasks/ArabicPOS_v4.py
@@ -5,84 +5,6 @@ from sklearn.metrics import f1_score
 from arabic_llm_benchmark.tasks.task_base import TaskBase
 
 
-mapTags = {
-    "UNK": "UNK",
-    "EOS": "EOS",
-    ".": "PUNC",
-    "NNP": "NOUN",
-    "JJR": "ADJ",
-    "CD": "NOUN",
-    "ADJ": "ADJ",
-    "adjective": "ADJ",
-    "JJ": "ADJ",
-    "MD": "ADJ",
-    "ADJF": "ADJ",
-    "ADV": "ADV",
-    "ADP": "ADV",
-    "adverb": "ADV",
-    "conjunction": "CONJ",
-    "CONJ": "CONJ",
-    "SCONJ": "CONJ",
-    "CCONJ": "CONJ",
-    "CC": "CONJ",
-    "DT": "DET",
-    "DET": "DET",
-    "hashtag": "HASH",
-    "NOUN": "NOUN",
-    "noun": "NOUN",
-    "N": "NOUN",
-    "NN": "NOUN",
-    "INTJ": "NOUN",
-    "PROPN": "NOUN",
-    "NEG": "PART",
-    "PART": "PART",
-    "IN": "PART",
-    "preposition": "PREP",
-    "P": "PREP",
-    "PREP": "PREP",
-    "PRP": "PREP",
-    "PRON": "PRON",
-    "pronoun": "PRON",
-    "REL": "PRON",
-    "DEM": "PRON",
-    "PUNC": "PUNC",
-    "punctuation": "PUNC",
-    "PUNCT": "PUNC",
-    "SYM": "PUNC",
-    "verb": "V",
-    "VERB": "V",
-    "V": "V",
-    "VB": "V",
-    "RB": "ADV",
-    "VBG": "V",
-    "VBZ": "V",
-    "PRO": "PRON",
-    "conj": "CONJ",
-    "punct": "PUNC",
-    "neg": "PART",
-    "pron": "PRON",
-    "prep": "PREP",
-    "COMP": "ADJ",
-    "interjection": "PART",
-    "number": "NOUN",
-    "MOD": "PART",
-    "NUM": "NOUN",
-    "determiner": "DET",
-    "negation": "PART",
-    "url": "URL",
-    "demonstrative": "PRON",
-    "particle": "PART",
-    "HASHTAG": "HASH",
-    "NPROP": "NOUN",
-    "EMOJI": "EMOJI",
-    ",": "PUNC",
-    "RELPRO": "PRON",
-    "X": "NOUN",
-    "MENTION": "MENTION",
-    "": "",
-}
-
-
 class ArabicPOSTask_v4(TaskBase):
     def __init__(self, **kwargs):
         super(ArabicPOSTask_v4, self).__init__(**kwargs)
@@ -111,18 +33,11 @@ class ArabicPOSTask_v4(TaskBase):
             p = p.replace('+NSUFF','').replace('DET+','')
 
             p = p.split()
-
             t = t.split()
-            #t = t.replace('+',' ').split()
-            # if(len(t)!=len(p)):
-            #     continue
             if len(p) < len(t):
                 for i in range(len(t) - len(p)):
                     p.append("")
 
-            # p = [mapTags[tag.replace('\'','')] for tag in p]
-            #print("PP1:",len(p),p)
-            #print("TT1:",len(t),t)
             hyp += p[: len(t)]
             ref += t
         #print("ph:",len(hyp),len(ref))
