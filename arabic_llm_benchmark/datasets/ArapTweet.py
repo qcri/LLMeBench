73dac10d712f034ad54d1c2399ced10bb8541d78
Firoj Alam, Scientist, QCRI <firojalam@users.noreply.github.com>
2023-06-20 15:03:02 +0300

    Add BLOOMZ asset for Gender ArapTweet (#108)
    * added asset zero-shot/bloomz/gender/GenderArapTweet
    
    * updated
    
    * Add train data loader in ArapTweet
    
    * Avoid duplicate names in train data loader
    
    * Rename file for consistent casing
    
    ---------
    
    Co-authored-by: Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>

--- arabic_llm_benchmark/datasets/ArapTweet.py
+++ arabic_llm_benchmark/datasets/ArapTweet.py
@@ -19,9 +19,50 @@ class ArapTweetDataset(DatasetBase):
 
     def load_data(self, data_path, no_labels=False):
         data = []
-        with open(data_path, "r") as fp:
-            for line_idx, line in enumerate(fp):
-                name, label = line.strip().split("\t")
-                data.append({"input": name, "label": label, "line_number": line_idx})
+        if "test" in data_path:
+            with open(data_path, "r") as fp:
+                for line_idx, line in enumerate(fp):
+                    name, label = line.strip().split("\t")
+                    data.append(
+                        {
+                            "input": name,
+                            "input_id": name,
+                            "label": label,
+                            "line_number": line_idx,
+                        }
+                    )
+        else:
+            user_ids = set()
+            with open(data_path, "r") as fp:
+                for line_idx, line in enumerate(fp):
+                    line = line.strip()
+
+                    # Ignore empty lines
+                    if len(line) == 0:
+                        continue
+
+                    arr = line.split("\t")
+
+                    # Ignore lines that do not have all columns
+                    if len(arr) != 6:
+                        continue
+
+                    # Do not add the same user twice
+                    user_id = arr[0].strip()
+                    if user_id in user_ids:
+                        continue
+                    user_ids.add(user_id)
+
+                    # Set `input_id` to name to deduplicate based on same name
+                    name = arr[1].strip()
+                    label = arr[3].strip()
+                    data.append(
+                        {
+                            "input": name,
+                            "label": label,
+                            "input_id": user_id,
+                            "line_number": line_idx,
+                        }
+                    )
 
         return data
