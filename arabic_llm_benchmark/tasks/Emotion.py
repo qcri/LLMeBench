0a2bea9e510dd688c3d8afbae985b624ae1a8af4
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2023-06-20 16:12:53 +0300

    Move random generator util to base

--- arabic_llm_benchmark/tasks/Emotion.py
+++ arabic_llm_benchmark/tasks/Emotion.py
@@ -1,6 +1,3 @@
-import random
-
-import numpy as np
 from sklearn.metrics import jaccard_score
 
 from arabic_llm_benchmark.tasks.task_base import TaskBase
@@ -9,9 +6,6 @@ from arabic_llm_benchmark.tasks.task_base import TaskBase
 class EmotionTask(TaskBase):
     def __init__(self, **kwargs):
         super(EmotionTask, self).__init__(**kwargs)
-
-    def create_random_binary_array(self, length):
-        return np.random.randint(low=0, high=2, size=(length,))
 
     def evaluate(self, true_labels, predicted_labels):
         predicted_labels = [
