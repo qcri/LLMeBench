db19049c66196ddd0ebc5166358089e0138ebc5d
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2023-06-20 15:51:17 +0300

    Format code

--- arabic_llm_benchmark/tasks/ArabicSegmentation.py
+++ arabic_llm_benchmark/tasks/ArabicSegmentation.py
@@ -12,32 +12,31 @@ class ArabicSegmentationTask_v4(TaskBase):
     def evaluate(self, true_labels, predicted_labels):
         # split sentence into words
         attrs = vars(self)
-        #print("P:",len(predicted_labels), " t:",len(true_labels))
+        # print("P:",len(predicted_labels), " t:",len(true_labels))
         hyp = []
         ref = []
         for t, p in zip(true_labels, predicted_labels):
-            
-            #print("P0:", p)
+            # print("P0:", p)
             if p == None:
                 # return unsegmented text!
                 p = t.replace("+", "").split()
             else:
                 p = p.split()
 
-            t = re.sub(r'[^ ]+[A-Za-z]+ ',' ',t)
+            t = re.sub(r"[^ ]+[A-Za-z]+ ", " ", t)
 
             t = t.split()
 
             if len(p) < len(t):
                 for i in range(len(t) - len(p)):
                     p.append("")
-            
-            #if(len(p)!=len(t)):
-            print("PP1:",len(p[:len(t)]),p[:len(t)])
-            print("TT1:",len(t),t)
-            
-            hyp += p[:len(t)]
+
+            # if(len(p)!=len(t)):
+            print("PP1:", len(p[: len(t)]), p[: len(t)])
+            print("TT1:", len(t), t)
+
+            hyp += p[: len(t)]
             ref += t
-        #print("ph:",len(hyp),hyp)
-        #print("tt:",len(ref),ref)
+        # print("ph:",len(hyp),hyp)
+        # print("tt:",len(ref),ref)
         return {"Macro F1": f1_score(ref, hyp, average="macro")}
--- assets/benchmark_v1/sequence_tagging_ner_pos_etc/segmentation_ChatGPT4_ZeroShot.py
+++ assets/benchmark_v1/sequence_tagging_ner_pos_etc/segmentation_ChatGPT4_ZeroShot.py
@@ -1,5 +1,6 @@
 import os
 import re
+
 from arabic_llm_benchmark.datasets import ArabicSegmentationDataset
 from arabic_llm_benchmark.models import GPTChatCompletionModel
 from arabic_llm_benchmark.tasks import ArabicSegmentationTask_v4
@@ -7,51 +8,59 @@ from arabic_llm_benchmark.tasks import ArabicSegmentationTask_v4
 
 def config():
     sets = [
-            ('egy', 'egy.seg/egy.data_5.test.src.sent'),
-            ('glf','glf.seg/glf.data_5.test.src.sent'),
-            ('mgr','mgr.seg/mgr.data_5.test.src.sent'),
-            ('lev','lev.seg/lev.data_5.test.src.sent'),
-            ('msa','WikiNewsTruth.txt')
-        ]
+        ("egy", "egy.seg/egy.data_5.test.src.sent"),
+        ("glf", "glf.seg/glf.data_5.test.src.sent"),
+        ("mgr", "mgr.seg/mgr.data_5.test.src.sent"),
+        ("lev", "lev.seg/lev.data_5.test.src.sent"),
+        ("msa", "WikiNewsTruth.txt"),
+    ]
     configs = []
     for name, testset in sets:
-        configs.append({
-            "name": name,
-            "config": {
-                "dataset": ArabicSegmentationDataset,
-                "dataset_args": {},
-                "task": ArabicSegmentationTask_v4,
-                "task_args": {},
-                "model": GPTChatCompletionModel,
-                "model_args": {
-                    "api_type": "azure",
-                    "api_version": "2023-03-15-preview",
-                    "api_base": os.environ["AZURE_API_URL"],
-                    "api_key": os.environ["AZURE_API_KEY"],
-                    "engine_name": os.environ["ENGINE_NAME"],
-                    # "class_labels": ["m", "f"],
-                    "max_tries": 3,
+        configs.append(
+            {
+                "name": name,
+                "config": {
+                    "dataset": ArabicSegmentationDataset,
+                    "dataset_args": {},
+                    "task": ArabicSegmentationTask_v4,
+                    "task_args": {},
+                    "model": GPTChatCompletionModel,
+                    "model_args": {
+                        "api_type": "azure",
+                        "api_version": "2023-03-15-preview",
+                        "api_base": os.environ["AZURE_API_URL"],
+                        "api_key": os.environ["AZURE_API_KEY"],
+                        "engine_name": os.environ["ENGINE_NAME"],
+                        # "class_labels": ["m", "f"],
+                        "max_tries": 3,
+                    },
+                    "general_args": {
+                        "data_path": "data/sequence_tagging_ner_pos_etc/segmentation/"
+                        + testset
+                    },
                 },
-                "general_args": {
-                    "data_path": "data/sequence_tagging_ner_pos_etc/segmentation/"+testset
-                },
-            }})
+            }
+        )
     return configs
 
 
 def prompt(input_sample):
     return [
-            {"role":"system","content": "You are a linguist that helps in annotating data."},
-            {
-                "role":"user",
-                "content":f"A word can be composed of one root and one or multiple affixed, segment the following sentence into its morphological constituents::\n{input_sample}\n"+"Output the results in a json format. {word: seg1+seg2+..}."
-            }
-        ]
+        {
+            "role": "system",
+            "content": "You are a linguist that helps in annotating data.",
+        },
+        {
+            "role": "user",
+            "content": f"A word can be composed of one root and one or multiple affixed, segment the following sentence into its morphological constituents::\n{input_sample}\n"
+            + "Output the results in a json format. {word: seg1+seg2+..}.",
+        },
+    ]
 
 
 def post_process(response):
     text = response["choices"][0]["message"]["content"]
     results = []
-    for t in text.split('\n')[1:-1]:
-        results.append(t.split(':')[1].replace('"','').replace(',',''))
-    return ' '.join(results)
+    for t in text.split("\n")[1:-1]:
+        results.append(t.split(":")[1].replace('"', "").replace(",", ""))
+    return " ".join(results)
