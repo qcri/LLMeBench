457ab6a7a7d8d1627d79eff847e99ae90b415631
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2023-06-21 12:29:54 +0300

    Update data path in HS asset

--- assets/benchmark_v1/factuality_disinformation_harmful_content/HateSpeech_ChatGPT_ZeroShot.py
+++ assets/benchmark_v1/factuality_disinformation_harmful_content/HateSpeech_ChatGPT_ZeroShot.py
@@ -22,7 +22,7 @@ def config():
             "max_tries": 3,
         },
         "general_args": {
-            "data_path": "data/sentiment_emotion_others/hate_speech/OSACT2020-sharedTask-test-tweets-labels.txt"
+            "data_path": "data/factuality_disinformation_harmful_content/hate_speech/OSACT2020-sharedTask-test-tweets-labels.txt"
         },
     }
 
