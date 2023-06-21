93681793c16449ed958f7aca10abd674b8566c6f
Fahim Dalvi <faimaduddin@hbku.edu.qa>
2023-06-21 11:09:16 +0300

    Add Diacritization dataset/task/asset (#128)
    * Add diacritizaton module
    
    * Update ArabicDiacritization.py
    
    Use undiacritized tokens as fall back for None results.
    
    * Format code
    
    * Add comments and minor fixes
    
    * More fixes to dataloader
    
    ---------
    
    Co-authored-by: Ahmed Abdelali <ahmed.abdelali@gmail.com>

---
+++ assets/benchmark_v1/sequence_tagging_ner_pos_etc/diacritization_ChatGPT_ZeroShot.py
@@ -0,0 +1,42 @@
+import os
+
+from arabic_llm_benchmark.datasets import ArabicDiacritizationDataset
+from arabic_llm_benchmark.models import GPTModel
+from arabic_llm_benchmark.tasks import ArabicDiacritizationTask
+
+
+def config():
+    return {
+        "dataset": ArabicDiacritizationDataset,
+        "dataset_args": {},
+        "task": ArabicDiacritizationTask,
+        "task_args": {},
+        "model": GPTModel,
+        "model_args": {
+            "api_type": "azure",
+            "api_version": "2023-03-15-preview",
+            "api_base": os.environ["AZURE_API_URL"],
+            "api_key": os.environ["AZURE_API_KEY"],
+            "engine_name": os.environ["ENGINE_NAME"],
+            "max_tries": 3,
+        },
+        "general_args": {
+            "data_path": "data/sequence_tagging_ner_pos_etc/diacritization/WikiNewsTruth.txt"
+        },
+    }
+
+
+def prompt(input_sample):
+    return {
+        "system_message": "You are an AI assistant that helps people find information.",
+        "messages": [
+            {
+                "sender": "user",
+                "text": f"Diacritize fully the following Arabic sentence: {input_sample}",
+            }
+        ],
+    }
+
+
+def post_process(response):
+    return response["choices"][0]["text"]
