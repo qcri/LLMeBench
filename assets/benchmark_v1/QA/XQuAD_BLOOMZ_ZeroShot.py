d2ef4a70a8be906026eac50b3d4b5ebbdbce86f4
Basel Mousi <bmousi@hbku.edu.qa>
2023-06-22 19:57:53 +0300

    Added bloomz asset for xquad

---
+++ assets/benchmark_v1/QA/XQuAD_BLOOMZ_ZeroShot.py
@@ -0,0 +1,33 @@
+import os
+
+from arabic_llm_benchmark.datasets import XQuADDataset
+from arabic_llm_benchmark.models import BLOOMPetalModel
+from arabic_llm_benchmark.tasks import QATask
+
+
+def config():
+    return {
+        "dataset": XQuADDataset,
+        "dataset_args": {},
+        "task": QATask,
+        "task_args": {},
+        "model": BLOOMPetalModel,
+        "model_args": {
+            "api_url": os.environ["API_URL"],
+            "max_tries": 5,
+        },
+        "general_args": {"data_path": "data/QA/xquad/xquad.ar.json"},
+    }
+
+
+def prompt(input_sample):
+    return {
+        "prompt": "Your task is to answer arabic questions based on a given context. Your answers should be extracted from the context." + "\n"
+        + f"Context: {input_sample['context']}\n"
+        + f"Question: {input_sample['question']}\n"
+        + "Answer: "
+    }
+
+
+def post_process(response):
+    return response["outputs"]
