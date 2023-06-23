da0a7915f4a3b1821544c36dd1d104c5b84feaa9
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2023-06-23 16:19:45 +0300

    Remove gold vs prediction length checking code, they will always be equal

--- arabic_llm_benchmark/tasks/Lemmatization.py
+++ arabic_llm_benchmark/tasks/Lemmatization.py
@@ -16,14 +16,4 @@ class LemmatizationTask(TaskBase):
         # Trim gold labels to lemmatized words
         true_labels = [t_l for _, t_l in true_labels]
 
-        len1 = len(true_labels)
-        len2 = len(predicted_labels)
-        if len1 < len2:
-            # Trim predicted_labels
-            predicted_labels = predicted_labels[len1]
-        elif len1 > len2:
-            # Pad predicted_labels
-            for j in range(len1 - len2):
-                predicted_labels.add("ERROR")
-
         return {"accuracy_score": accuracy_score(true_labels, predicted_labels)}
