88f3e61dae645aae5b909e823272c52cd21078d2
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2023-06-24 10:29:13 +0300

    More fixes

--- arabic_llm_benchmark/tasks/Lemmatization.py
+++ arabic_llm_benchmark/tasks/Lemmatization.py
@@ -8,15 +8,13 @@ class LemmatizationTask(TaskBase):
         super(LemmatizationTask, self).__init__(**kwargs)
 
     def evaluate(self, true_labels, predicted_labels):
-        # Hack to get unlemmatized word in case of failure
-        predicted_labels = [p_l for _, p_l in predicted_labels]
-
         # Replace failed predictions with unlemmatized word
         for idx, pred in enumerate(predicted_labels):
             if pred is None:
-                predicted_labels[idx] = true_labels[idx][0]
+                predicted_labels[idx] = (None, true_labels[idx][0])
 
-        # Trim gold labels to lemmatized words
+        # Trim labels to relevant pieces
         true_labels = [t_l for _, t_l in true_labels]
+        predicted_labels = [p_l for _, p_l in predicted_labels]
 
         return {"accuracy_score": accuracy_score(true_labels, predicted_labels)}
