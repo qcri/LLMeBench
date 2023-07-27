4b56559a9e33e5e087438ca43f887db01aedd960
Basel Mousi <bmousi@hbku.edu.qa>
2023-07-27 13:50:30 +0300

    modified prompt for few shot POS

--- assets/benchmark_v1/sequence_tagging_ner_pos_etc/POS_GPTChatCompletion_FewShot.py
+++ assets/benchmark_v1/sequence_tagging_ner_pos_etc/POS_GPTChatCompletion_FewShot.py
@@ -143,21 +143,15 @@ def few_shot_prompt(input_sample, base_prompt, examples):
         label = example["label"]
         sample = list(zip(tokens.split(), label.split()))
         output_prompt = (
-            output_prompt + f"Sentence: {tokens}\nLabels: {sample}\n"
+            output_prompt + f"Sentence: {tokens.split()}" + "\n" + f"Labels: {sample}" + "\n"
         )
-    output_prompt = output_prompt + f"Sentence: {input_sample}\n" + "Labels:"
+    output_prompt = output_prompt + f"Sentence: {input_sample.split()}" + "\n" + "Labels:"
     return output_prompt
 
 
 def prompt(input_sample, examples): 
-    base_prompt = f'Please provide the POS tags for each word in the input sentence. The input will be a list of words in the sentence. \
-                    The output format should be a list of tuples, where each tuple consists of a word from the input text and its \
-                    corresponding POS tag label from the tag label set: \
-                    ["ABBREV", "ADJ", "ADV", "CASE", "CONJ", "DET", "EMOT", "FOREIGN", "FUT_PART", "HASH", "MENTION", "NEG_PART", "NOUN", \
-                    "NSUFF", "NUM", "PART", "PREP", "PROG_PART", "PRON", "PUNC", "URL", "V"].\
-                    Note: Your response should include only a list of tuples, in the order that the words appear in the input sentence, \
-                    with each tuple containing the corresponding POS tag label for a word.'
-
+    base_prompt = f'Please provide the POS tags for each word in the input sentence. The input will be a list of words in the sentence. The output format should be a list of tuples, where each tuple consists of a word from the input text and its corresponding POS tag label from the tag label set: ["ABBREV", "ADJ", "ADV", "CASE", "CONJ", "DET", "EMOT", "FOREIGN", "FUT_PART", "HASH", "MENTION", "NEG_PART", "NOUN", "NSUFF", "NUM", "PART", "PREP", "PROG_PART", "PRON", "PUNC", "URL", "V"]. Note: Your response should include only a list of tuples, in the order that the words appear in the input sentence, with each tuple containing the corresponding POS tag label for a word.'
+    
     return [
             {"role":"system","content": "You are a linguist that helps in annotating data."},
             {
