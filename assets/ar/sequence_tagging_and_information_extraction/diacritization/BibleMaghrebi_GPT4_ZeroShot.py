58a4fc664b23abc8082ba3bd2d08f45e5afa6300
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2023-09-06 22:30:46 +0300

    Remove all hardcoded model config options from assets

--- assets/ar/sequence_tagging_and_information_extraction/diacritization/BibleMaghrebi_GPT4_ZeroShot.py
+++ assets/ar/sequence_tagging_and_information_extraction/diacritization/BibleMaghrebi_GPT4_ZeroShot.py
@@ -22,11 +22,6 @@ def config():
                     "task_args": {},
                     "model": OpenAIModel,
                     "model_args": {
-                        "api_type": "azure",
-                        "api_version": "2023-03-15-preview",
-                        "api_base": os.environ["AZURE_API_URL"],
-                        "api_key": os.environ["AZURE_API_KEY"],
-                        "engine_name": os.environ["ENGINE_NAME"],
                         "max_tries": 3,
                     },
                     "general_args": {
