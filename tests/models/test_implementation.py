357b325dc3e8e3caae28e138f26374ec78eb3929
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2023-09-07 11:54:34 +0300

    Add tests for base class constructor calls

---
+++ tests/models/test_implementation.py
@@ -0,0 +1,69 @@
+import ast
+import inspect
+import unittest
+
+from pathlib import Path
+
+import llmebench.models as models
+
+
+class TestModelImplementation(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        # Search for all implemented models
+        framework_dir = Path("llmebench")
+        cls.models = set([m[1] for m in inspect.getmembers(models, inspect.isclass)])
+
+    def test_base_constructor(self):
+        "Test if all models also call the base class constructor"
+
+        for model in self.models:
+            with self.subTest(msg=model.__name__):
+                tree = ast.parse(inspect.getsource(model))
+                constructors = list(
+                    n
+                    for n in ast.walk(tree)
+                    if isinstance(n, ast.FunctionDef) and n.name == "__init__"
+                )
+                self.assertLessEqual(
+                    len(constructors), 1, "Multiple constructors found"
+                )
+
+                if len(constructors) == 0:
+                    # No constructor, base will be called by default
+                    continue
+
+                constructor = constructors[0]
+
+                # Collect all function calls inside the constructor
+                fn_calls = list(
+                    n
+                    for n in ast.walk(constructor)
+                    if isinstance(n, ast.Call) and isinstance(n.func, ast.Attribute)
+                )
+
+                # For each, check if there is something of the form super(...).__init(...)
+                def is_base_constructor_call(node):
+                    fn_call = node.func
+                    if not isinstance(fn_call.value, ast.Call):
+                        return False
+                    if not fn_call.value.func.id == "super":
+                        return False
+                    if not fn_call.attr == "__init__":
+                        return False
+
+                    return True
+
+                filtered_fn_calls = list(filter(is_base_constructor_call, fn_calls))
+
+                self.assertEqual(
+                    len(filtered_fn_calls), 1, "Call to base class constructor missing"
+                )
+
+                self.assertTrue(
+                    any(
+                        isinstance(k.value, ast.Name) and k.value.id == "kwargs"
+                        for k in filtered_fn_calls[0].keywords
+                    ),
+                    "kwargs not passed to the base class constructor",
+                )
