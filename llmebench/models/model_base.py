35b2464e2b0afc8edaae55f536b8347bf220ea26
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2023-09-11 09:13:02 +0300

    Add documentation for model base

--- llmebench/models/model_base.py
+++ llmebench/models/model_base.py
@@ -34,7 +34,7 @@ class ModelBase(object):
     max_tries : int, defaults to 5
         Defines how many retries are allowed per-sample in case of failure.
         Failure is defined by `retry_exceptions`.
-    retru_exceptions : tuple
+    retry_exceptions : tuple
         Tuple of exceptions on which the framework should retry the request
         for any given sample. Specific exceptions should be included by the
         implementing class, such as HTTP Request failures (in case of HTTP-
@@ -42,13 +42,16 @@ class ModelBase(object):
 
     Methods
     -------
-    prompt(**kwargs):
+    prompt(processed_input):
         Method that takes inputs from an asset and makes the actual request
         to the underlying model inference API.
 
     summarize_response(response):
         Method that takes a model response and summarizes it into a simpler
         form
+
+    run_model(processed_input):
+        Wrapper that calls the `prompt` method and captures exceptions
     """
 
     def __init__(self, max_tries=5, retry_exceptions=(), **kwargs):
@@ -64,14 +67,68 @@ class ModelBase(object):
         )(self.prompt)
 
     @abstractmethod
-    def prompt(self, **kwargs):
+    def prompt(self, processed_input):
+        """
+        Method that implements communication to the underlying model
+
+        Arguments
+        ---------
+        processed_input : dict
+            Input from an asset. The structure of this will be dependent
+            on a specific model implementation, and must be documented by
+            the class implementation itself
+
+        Returns
+        -------
+        response : mixed
+            Response form the underlying model API
+
+        Notes
+        -----
+        Ideally, this method will never be called directly, but through the
+        `run_model` wrapper which takes care of returning the output in a
+        consistent manner and also handles errors/exceptions.
+        """
         pass
 
     @abstractmethod
     def summarize_response(self, response):
+        """
+        Method that summarizes/simplifies a model's response
+
+        Arguments
+        ---------
+        response : mixed
+            Response from `prompt()`
+
+        Returns
+        -------
+        simplified_response : mixed
+            Should ideally be a short string that summarizes the model's response
+            (e.g. only the actual label instead of scores and other metadata). Will
+            be saved in the summary file for quick debugging. If the response is not
+            simplifiable, return the response object as is.
+        """
         pass
 
     def run_model(self, processed_input):
+        """
+        Wrapper that calls the `prompt` method and captures exceptions
+
+        Arguments
+        ---------
+        processed_input : dict
+            Input from an asset. The structure of this will be dependent
+            on a specific model implementation, and must be documented by
+            the class implementation itself
+
+        Returns
+        -------
+        response : dict
+            Returns a dictionary with the key "response" holding the model's
+            response, or "failure_exception" with the error that occurred when
+            using the model
+        """
         try:
             response = self.prompt(processed_input)
             return {"response": response}
