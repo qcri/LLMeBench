fd260a682ff7731a9fbf8e31b7dfe070b965ba17
Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>
2023-09-25 10:41:32 +0300

    Add FASTCHAT* environment variables for model config

--- llmebench/models/FastChat.py
+++ llmebench/models/FastChat.py
@@ -5,20 +5,20 @@ from llmebench.models.OpenAI import OpenAIModel
 
 class FastChatModel(OpenAIModel):
     def __init__(self, api_base=None, api_key=None, model_name=None, **kwargs):
-        api_base = api_base or os.getenv("OPENAI_API_BASE")
-        api_key = api_key or os.getenv("OPENAI_API_KEY")
-        model_name = model_name or os.getenv("OPENAI_MODEL")
+        api_base = api_base or os.getenv("FASTCHAT_API_BASE")
+        api_key = api_key or os.getenv("FASTCHAT_API_KEY")
+        model_name = model_name or os.getenv("FASTCHAT_MODEL")
         if api_base is None:
             raise Exception(
-                "API url must be provided as model config or environment variable (`OPENAI_API_BASE`)"
+                "API url must be provided as model config or environment variable (`FASTCHAT_API_BASE`)"
             )
         if api_key is None:
             raise Exception(
-                "API url must be provided as model config or environment variable (`OPENAI_API_KEY`)"
+                "API url must be provided as model config or environment variable (`FASTCHAT_API_KEY`)"
             )
         if model_name is None:
             raise Exception(
-                "API url must be provided as model config or environment variable (`OPENAI_MODEL`)"
+                "API url must be provided as model config or environment variable (`FASTCHAT_MODEL`)"
             )
         # checks for valid config settings)
         super(FastChatModel, self).__init__(
