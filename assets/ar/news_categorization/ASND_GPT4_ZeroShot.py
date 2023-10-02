276f75afd285b818273b07f559ae7bf26303fad5
Maram Hasanain <maramhasanain@gmail.com>
2023-10-02 16:13:13 +0300

    Add a new asset for ASND news categorization with Arabic prompt (#184)
    * Add files via upload
    
    * Update NewsCat_ASND_GPTChatCompletion_ZeroShot_Arabic_instruct.py
    
    * Updated prompt
    
    * Update asset with metadata, remove obsolete code and format
    
    ---------
    
    Co-authored-by: Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>

--- assets/ar/news_categorization/ASND_GPT4_ZeroShot.py
+++ assets/ar/news_categorization/ASND_GPT4_ZeroShot.py
@@ -7,8 +7,8 @@ def metadata():
     return {
         "author": "Arabic Language Technologies, QCRI, HBKU",
         "model": "gpt-4-32k (version 0314)",
-        "description": "GPT4 32k tokens model hosted on Azure, using the ChatCompletion API. API version '2023-03-15-preview'.",
-        "scores": {"Macro-F1": "0.667"},
+        "description": "GPT4 32k tokens model hosted on Azure, using the ChatCompletion API. API version '2023-03-15-preview'. Uses an prompt specified in Arabic.",
+        "scores": {"Macro-F1": "0.739"},
     }
 
 
@@ -17,40 +17,23 @@ def config():
         "dataset": ASNDDataset,
         "task": NewsCategorizationTask,
         "model": OpenAIModel,
-        "model_args": {
-            "class_labels": [
-                "crime-war-conflict",
-                "spiritual",
-                "health",
-                "politics",
-                "human-rights-press-freedom",
-                "education",
-                "business-and-economy",
-                "art-and-entertainment",
-                "others",
-                "science-and-technology",
-                "sports",
-                "environment",
-            ],
-            "max_tries": 30,
-        },
     }
 
 
 def prompt(input_sample):
     prompt_string = (
-        f"Categorize the following tweet into one of the following categories: "
-        f"crime-war-conflict, spiritual, health, politics, human-rights-press-freedom, "
-        f"education, business-and-economy, art-and-entertainment, others, "
-        f"science-and-technology, sports, environment\n"
-        f"\ntweet: {input_sample}"
-        f"\ncategory: \n"
+        f"صنف التغريدة التالية إلى واحدة من الفئات التالية: "
+        f"جريمة-حرب-صراع ، روحي-ديني ، صحة ، سياسة ، حقوق-الإنسان-حرية-الصحافة ، "
+        f"تعليم ، أعمال-اقتصاد ، فن-ترفيه ، أخرى ، "
+        f"علوم-تكنولوجيا ، رياضة ، بيئة\n"
+        f"\nالتغريدة: {input_sample}"
+        f"\nالفئة: \n"
     )
 
     return [
         {
             "role": "system",
-            "content": "You are an expert tweet annotator and know how to categorize news tweet.",
+            "content": "أنت خبير في تصنيف التغريدات وتعرف كيف تصنف تغريدات الأخبار.",
         },
         {
             "role": "user",
@@ -62,11 +45,31 @@ def prompt(input_sample):
 def post_process(response):
     label = response["choices"][0]["message"]["content"]
 
-    label_fixed = label.lower()
-    label_fixed = label_fixed.replace("category: ", "")
-    label_fixed = label_fixed.replace("text:", "")
-    if label_fixed != "true" or label_fixed != "false":
-        if len(label_fixed.split()) > 1:
-            label_fixed = label_fixed.split()[0]
+    if "جريمة-حرب-صراع" in label or "صراع-حرب" in label:
+        label_fixed = "crime-war-conflict"
+    elif "روحي" in label or "ديني" in label:
+        label_fixed = "spiritual"
+    elif "صحة" in label:
+        label_fixed = "health"
+    elif "سياسة" in label:
+        label_fixed = "politics"
+    elif "حقوق-الإنسان-حرية-الصحافة" in label:
+        label_fixed = "human-rights-press-freedom"
+    elif "تعليم" in label:
+        label_fixed = "education"
+    elif "أعمال-و-اقتصاد" in label or "أعمال" in label or "اقتصاد" in label:
+        label_fixed = "business-and-economy"
+    elif "فن-و-ترفيه" in label or "ترفيه" in label:
+        label_fixed = "art-and-entertainment"
+    elif "أخرى" in label:
+        label_fixed = "others"
+    elif "علم-و-تكنولوجيا" in label or "علوم" in label or "تكنولوجيا" in label:
+        label_fixed = "science-and-technology"
+    elif "رياضة" in label:
+        label_fixed = "sports"
+    elif "بيئة" in label:
+        label_fixed = "environment"
+    else:
+        label_fixed = "others"
 
     return label_fixed
