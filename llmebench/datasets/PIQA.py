5daaf5958b1a34d791dbd2a2f9dc6863e156e3b7
bsabri <bsabri@yahoo.com>
2023-10-14 19:23:53 +0300

    added PIQA dataset

--- llmebench/datasets/PIQA.py
+++ llmebench/datasets/PIQA.py
@@ -3,8 +3,6 @@ import pandas as pd
 
 from llmebench.datasets.dataset_base import DatasetBase
 from llmebench.tasks import TaskType
-
-
 class PIQADataset(DatasetBase):
     def __init__(self, **kwargs):
         super(PIQADataset, self).__init__(**kwargs)
