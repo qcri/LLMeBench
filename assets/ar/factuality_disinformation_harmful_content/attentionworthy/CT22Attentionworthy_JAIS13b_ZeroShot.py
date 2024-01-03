491fd23743477ca21d61a95a891d9568e16721f6
Arid Hasan <arid.hasan.h@gmail.com>
2024-01-02 21:01:26 -0400

    Add factuality, disinformation, harmful content assets for JAIS

---
+++ assets/ar/factuality_disinformation_harmful_content/attentionworthy/CT22Attentionworthy_JAIS13b_ZeroShot.py
@@ -0,0 +1,73 @@
+from llmebench.datasets import CT22AttentionworthyDataset
+from llmebench.models import FastChatModel
+from llmebench.tasks import AttentionworthyTask
+
+
+def metadata():
+    return {
+        "author": "Arabic Language Technologies, QCRI, HBKU",
+        "model": "JAIS-13b",
+        "description": "Locally hosted JAIS-13b-chat model using FastChat.",
+        "scores": {"Macro-F1": ""},
+    }
+
+
+def config():
+    return {
+        "dataset": CT22AttentionworthyDataset,
+        "task": AttentionworthyTask,
+        "model": FastChatModel,
+        "model_args": {
+            "class_labels": [
+                "yes_discusses_action_taken",
+                "harmful",
+                "yes_discusses_cure",
+                "yes_asks_question",
+                "no_not_interesting",
+                "yes_other",
+                "yes_blame_authorities",
+                "yes_contains_advice",
+                "yes_calls_for_action",
+            ],
+            "max_tries": 30,
+        },
+        "general_args": {"test_split": "ar"},
+    }
+
+
+def prompt(input_sample):
+    base_prompt = (
+        f'Annotate "tweet" into one of the following categories: yes_discusses_action_taken, harmful, yes_discusses_cure, yes_asks_question, no_not_interesting, yes_other, yes_blame_authorities, yes_contains_advice, yes_calls_for_action\n\n'
+        f"tweet: {input_sample}\n"
+        f"label: \n"
+    )
+    return [
+        {
+            "role": "user",
+            "content": base_prompt,
+        },
+    ]
+
+
+def post_process(response):
+    label = response["choices"][0]["message"]["content"]
+
+    label = (
+        label.lower()
+        .replace(" - ", ", ")
+        .replace(",", "")
+        .replace(".", "")
+        .replace("label:", "")
+    )
+    label = label.strip()
+    # label = re.sub("\s+", "_", label)
+    if label.startswith("no"):
+        label_fixed = "no_not_interesting"
+    elif label == "yes_discusses_covid-19_vaccine_side_effects":
+        label_fixed = "yes_discusses_cure"
+    elif label == "yes_harmful":
+        label_fixed = "harmful"
+    elif label.startswith("yes"):
+        label_fixed = label
+
+    return label_fixed
---
+++ assets/ar/factuality_disinformation_harmful_content/hate_speech/OSACT4SubtaskB_JAIS13b_ZeroShot.py
@@ -0,0 +1,51 @@
+from llmebench.datasets import OSACT4SubtaskBDataset
+from llmebench.models import FastChatModel
+from llmebench.tasks import HateSpeechTask
+
+
+def metadata():
+    return {
+        "author": "Arabic Language Technologies, QCRI, HBKU",
+        "model": "JAIS-13b",
+        "description": "Locally hosted JAIS-13b-chat model using FastChat.",
+        "scores": {"Macro-F1": ""},
+    }
+
+
+def config():
+    return {
+        "dataset": OSACT4SubtaskBDataset,
+        "task": HateSpeechTask,
+        "model": FastChatModel,
+        "model_args": {
+            "class_labels": ["HS", "NOT_HS"],
+            "max_tries": 3,
+        },
+    }
+
+
+def prompt(input_sample):
+    base_prompt = (
+        f'Given the following tweet, label it as "HS" or "NOT_HS" based on the content of the tweet: \n {input_sample}'
+    )
+    return [
+        {
+            "role": "user",
+            "content": base_prompt,
+        },
+    ]
+
+
+def post_process(response):
+    out = response["choices"][0]["message"]["content"]
+    j = out.find(".")
+    if j > 0:
+        out = out[0:j]
+
+    if "not_hate_speech" in out or "no_hate_speech" in out or "NOT_HS" == out:
+        out = "NOT_HS"
+    elif "hate_speech" in out or "HS" == out:
+        out = "HS"
+    else:
+        out = None
+    return out
