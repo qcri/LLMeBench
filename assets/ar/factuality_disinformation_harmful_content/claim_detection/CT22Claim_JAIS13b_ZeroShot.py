f0f20623e971b8b1c89d6749057364073c031c59
Arid Hasan <arid.hasan.h@gmail.com>
2024-01-02 21:01:26 -0400

    Add factuality, disinformation, harmful content assets for JAIS

---
+++ assets/ar/factuality_disinformation_harmful_content/claim_detection/CT22Claim_JAIS13b_ZeroShot.py
@@ -0,0 +1,56 @@
+from llmebench.datasets import CT22ClaimDataset
+from llmebench.models import FastChatModel
+from llmebench.tasks import ClaimDetectionTask
+
+
+def metadata():
+    return {
+        "author": "Arabic Language Technologies, QCRI, HBKU",
+        "model": "JAIS-13b",
+        "description": "Locally hosted JAIS-13b-chat model using FastChat.",
+        "scores": {"Macro-F1": ""},
+    }
+
+
+def config():
+    return {
+        "dataset": CT22ClaimDataset,
+        "task": ClaimDetectionTask,
+        "model": FastChatModel,
+        "model_args": {
+            "class_labels": ["0", "1"],
+            "max_tries": 30,
+        },
+        "general_args": {"test_split": "ar"},
+    }
+
+
+def prompt(input_sample):
+    base_prompt = (
+        f"Given the following tweet, please identify if it contains a claim. If it does, annotate 'yes', if it does not, annotate 'no'\n\n"
+        f"tweet: {input_sample}\n"
+        f"label: \n"
+    )
+    return [
+        {
+            "role": "user",
+            "content": base_prompt,
+        },
+    ]
+
+
+def post_process(response):
+    label = response["choices"][0]["message"]["content"]
+
+    label = label.replace("label:", "").strip()
+
+    if "label: " in label:
+        arr = label.split("label: ")
+        label = arr[1].strip()
+
+    if label == "yes" or label == "the sentence contains a factual claim":
+        label_fixed = "1"
+    if label == "no":
+        label_fixed = "0"
+
+    return label_fixed
---
+++ assets/ar/factuality_disinformation_harmful_content/offensive_language/OSACT4SubtaskA_JAIS13b_ZeroShot.py
@@ -0,0 +1,44 @@
+from llmebench.datasets import OSACT4SubtaskADataset
+from llmebench.models import FastChatModel
+from llmebench.tasks import OffensiveTask
+
+
+def metadata():
+    return {
+        "author": "Arabic Language Technologies, QCRI, HBKU",
+        "model": "JAIS-13b",
+        "description": "Locally hosted JAIS-13b-chat model using FastChat.",
+        "scores": {"Macro-F1": ""},
+    }
+
+
+def config():
+    return {
+        "dataset": OSACT4SubtaskADataset,
+        "task": OffensiveTask,
+        "model": FastChatModel,
+        "model_args": {
+            "class_labels": ["OFF", "NOT_OFF"],
+            "max_tries": 3,
+        },
+    }
+
+
+def prompt(input_sample):
+    base_prompt = (
+        f'if the following Arabic sentence is offensive, just say "OFF", otherwise, say just "NOT_OFF" without explanation: \n {input_sample}'
+    )
+    return [
+        {
+            "role": "user",
+            "content": base_prompt,
+        },
+    ]
+
+
+def post_process(response):
+    out = response["choices"][0]["message"]["content"]
+    j = out.find(".")
+    if j > 0:
+        out = out[0:j]
+    return out
