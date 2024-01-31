891654db9e081d795a63e89c2636c5d89dbd3371
Arid Hasan <18038960+AridHasan@users.noreply.github.com>
2024-01-31 07:22:31 -0400

    Add Jais ZeroShot assets for Arabic QA tasks (#258)
    * Add JAIS13b for arabic QA
    
    * Format code and minor metadata changes
    
    ---------
    
    Co-authored-by: Fahim Imaduddin Dalvi <faimaduddin@hbku.edu.qa>

---
+++ assets/ar/QA/MLQA_JAIS13b_ZeroShot.py
@@ -0,0 +1,36 @@
+from llmebench.datasets import MLQADataset
+from llmebench.models import FastChatModel
+from llmebench.tasks import QATask
+
+
+def metadata():
+    return {
+        "author": "Arabic Language Technologies, QCRI, HBKU",
+        "model": "Jais-13b-chat",
+        "description": "Locally hosted Jais-13b-chat model using FastChat.",
+    }
+
+
+def config():
+    return {
+        "dataset": MLQADataset,
+        "task": QATask,
+        "model": FastChatModel,
+        "model_args": {
+            "max_tries": 50,
+        },
+    }
+
+
+def prompt(input_sample):
+    base_prompt = f"Your task is to answer questions in Arabic based on a given context.\nNote: Your answers should be spans extracted from the given context without any illustrations.\nYou don't need to provide a complete answer\nContext:{input_sample['context']}\nQuestion:{input_sample['question']}\nAnswer:"
+    return [
+        {
+            "role": "user",
+            "content": base_prompt,
+        },
+    ]
+
+
+def post_process(response):
+    return response["choices"][0]["message"]["content"]
