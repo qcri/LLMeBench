b5043e154037ad7cd7f6fc8ec1c9ce751df2937b
Firoj Alam <firojalam@gmail.com>
2024-05-15 16:28:13 +0200

    updated fanar model scripts

--- llmebench/models/__init__.py
+++ llmebench/models/__init__.py
@@ -2,5 +2,5 @@ from .FastChat import FastChatModel
 from .HuggingFaceInferenceAPI import HuggingFaceInferenceAPIModel, HuggingFaceTaskTypes
 from .OpenAI import LegacyOpenAIModel, OpenAIModel
 from .Petals import PetalsModel
-from .VLLM import VLLMModel
 from .Random import RandomModel
+from .VLLM import VLLMModel
