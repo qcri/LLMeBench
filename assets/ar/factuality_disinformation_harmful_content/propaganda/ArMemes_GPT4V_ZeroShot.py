deb82e75d269aa35cf7da68ea0e995427e28bb1b
Firoj Alam <firojalam@gmail.com>
2024-06-03 10:42:09 +0200

    GPT-4 vision, dataset and assests added

---
+++ assets/ar/factuality_disinformation_harmful_content/propaganda/ArMemes_GPT4V_ZeroShot.py
@@ -0,0 +1,105 @@
+import json
+
+from llmebench.datasets import ArMemesDataset
+from llmebench.models import OpenAIModel
+from llmebench.tasks import ClassificationTask
+
+
+def metadata():
+    return {
+        "author": "Arabic Language Technologies, QCRI, HBKU",
+        "model": "gpt-4-v",
+        "description": "GPT4-o model hosted on Azure, using the ChatCompletion API. API version '2024-02-15-preview'.",
+        "scores": {"Micro-F1": ""},
+    }
+
+
+def config():
+    return {
+        "dataset": ArMemesDataset,
+        "task": ClassificationTask,
+        "model": OpenAIModel,
+        "model_args": {
+            "class_labels": ["not_propaganda", "propaganda", "not_meme", "other"],
+            "max_tries": 30,
+        },
+    }
+
+
+def prompt(input_sample):
+    base64_image = input_sample["image"]
+    t_text = input_sample["text"]
+    prompt = (
+        "You are an expert social media image analyzer specializing in identifying propaganda in Arabic contexts. "
+        "I will provide you with Arabic memes and the text extracted from these images. Your task is to briefly analyze them. "
+        "To accurately perform this task, you will: (a) Explicitly focus on the image content to understand the context and provide a meaningful description and "
+        "(b) pay close attention to the extracted text to enrich your description and support your analysis. "
+        'Finally, provide response in valid JSON format with two fields with a format: {"description": "text", "classification": "propaganda"}. Output only json. '
+        'The "description" should be very short in maximum 100 words and "classification" label should be "propaganda" or "not-propaganda" or "not-meme" or "other". '
+        "Note, other is a category, which is used to label the image that does not fall in any of the previous category."
+    )
+
+    return [
+        {
+            "role": "user",
+            "content": [
+                {
+                    "type": "text",
+                    "text": prompt,
+                },
+                {
+                    "type": "image_url",
+                    "image_url": {
+                        "url": f"data:image/jpeg;base64,{base64_image}",
+                    },
+                },
+                {
+                    "type": "text",
+                    "text": f"Text extracted: {t_text}",
+                },
+            ],
+        }
+    ]
+
+
+def preprocess_string(json_string, return_json=True):
+    # Replace single quotes with double quotes
+    json_string = json_string.strip().replace("\n", "")
+
+    # Remove trailing commas
+    json_string = json_string.replace("json", "")
+    json_string = json_string.replace("```", "")
+
+    # Fix double quotes within string values
+    decoder = json.JSONDecoder(
+        object_pairs_hook=lambda pairs: {
+            k: v.replace('\\"', '"') if isinstance(v, str) else v for k, v in pairs
+        }
+    )
+    json_data, _ = decoder.raw_decode(json_string)
+
+    if return_json:
+        return json_data
+    else:
+        # Return the preprocessed string
+        return json.dumps(json_data)
+
+
+def parseLLMJsonResponse(response_txt: str):
+    import re
+
+    obj = {}
+    parts = re.findall(
+        r"""(?:\"|\')([^"]*)(?:\"|\')(?=:)(?:\:\s*)(?:\")?(true|false|[-0-9]+[\.]*[\d]*(?=,)|[0-9a-zA-Z\(\)\@\:\,\/\!\+\-\.\$\ \\\']*)(?:\")?""",
+        response_txt,
+    )
+    for k, v in parts:
+        obj[k.strip()] = v
+    return obj
+
+
+def post_process(response):
+    t_message = parseLLMJsonResponse(response["choices"][0]["message"]["content"])
+    pred_label = t_message["classification"].lower()
+
+    return pred_label
