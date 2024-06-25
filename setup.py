f3338f824e079e6f53efe149d48008bcea576825
Firoj Alam <firojalam@gmail.com>
2024-06-25 22:51:57 +0300

    debug setup.py

--- setup.py
+++ setup.py
@@ -10,10 +10,6 @@ class PostInstallCommand(install):
         subprocess.run(["python", "-m", "nltk.downloader", "punkt"], check=True)
 
 setup(
-    setup_requires=['nltk'],
-    install_requires=[
-        'nltk==3.8.1',
-    ],
     cmdclass={
         'install': PostInstallCommand,
     },
