c07478cfeec6d9ef4a9490c96c82c295c8df09c8
Arid Hasan <arid.hasan.h@gmail.com>
2024-07-03 21:54:46 -0300

    asset MultiNativQA-Llama3 for Nepali

---
+++ assets/ne/QA/MultiNativQA_Llama3_8b_ZeroShot.py
@@ -0,0 +1,59 @@
+import json
+
+from llmebench.datasets import MultiNativQADataset
+from llmebench.models import AzureModel
+from llmebench.tasks import MultiNativQATask
+
+
+def metadata():
+    return {
+        "author": "Arabic Language Technologies, QCRI, HBKU",
+        "model": "LLama 3 8b",
+        "description": "Deployed on Azure.",
+        "scores": {},
+    }
+
+
+def config():
+    return {
+        "dataset": MultiNativQADataset,
+        "task": MultiNativQATask,
+        "model": AzureModel,
+        "general_args": {"test_split": "nepali_np"},
+    }
+
+
+def prompt(input_sample):
+
+    # Define the question prompt
+    question_prompt = f"""
+    Please use your expertise to answer the following Nepali question. You should only Answer in Nepali. 
+    Provide your response in the following JSON format: {{"answer": "your answer"}}. 
+    Please provide JSON output only. No additional text.
+
+    Question: {input_sample}
+    Answer: 
+    """
+
+    # Define the assistant prompt
+    assistant_prompt = """
+    I am an Nepali AI assistant specialized in providing detailed and accurate answers across various fields. I aim to deliver clear, concise, and relevant information. How can I assist you today?
+    """
+
+    return [
+        {
+            "role": "assistant",
+            "content": assistant_prompt,
+        },
+        {
+            "role": "user",
+            "content": question_prompt,
+        },
+    ]
+
+
+def post_process(response):
+    data = response["output"]
+    response = json.loads(data)
+    answer = response["answer"]
+    return answer
