d7db797b1f45100ddff970395744ddcc4db2ba58
Firoj Alam <firojalam@gmail.com>
2024-07-21 12:49:11 +0300

    fixed formatting issue

--- llmebench/models/__init__.py
+++ llmebench/models/__init__.py
@@ -6,4 +6,3 @@ from .OpenAI import LegacyOpenAIModel, OpenAIModel
 from .Petals import PetalsModel
 from .Random import RandomModel
 from .VLLM import VLLMModel
-from .Anthropic import AnthropicModel
