a2a681e98e1c7a76d8d947ce0d65bdaeddb8f0bf
Arid Hasan <18038960+AridHasan@users.noreply.github.com>
2024-09-07 05:08:46 -0300

    update mistral prompt MNQA AR (#344)
    * update mistral prompt MNQA AR
    
    * update mistral prompt MNQA AR
    
    * update mistral prompt MNQA AR

--- assets/ar/QA/MultiNativQA_Mistral_7b_ZeroShot.py
+++ assets/ar/QA/MultiNativQA_Mistral_7b_ZeroShot.py
@@ -19,6 +19,7 @@ def config():
         "dataset": MultiNativQADataset,
         "task": MultiNativQATask,
         "model": AzureModel,
+        "general_args": {"test_split": "arabic"},
     }
 
 
@@ -27,17 +28,17 @@ def prompt(input_sample):
     # Define the question prompt
     # Provide your response in the following JSON format and provide JSON output only. No additional text.
     question_prompt = f"""
-    Please use your expertise to answer the following Arabic Question. You should 'Answer' only in Arabic. 
-    
+    Please use your expertise to answer the following Arabic question. Answer in Arabic and rate your confidence level from 1 to 10.
+    Provide your response in the following JSON format: {{"answer": "your answer", "score": your confidence score}}.
+    Please provide JSON output only. No additional text. Answer should be limited to less or equal to {input_sample['length']} words.
 
-    Question: {input_sample}
-
-    Answer: 
+    Question: {input_sample['question']}
     """
 
     # Define the assistant prompt
     assistant_prompt = """
-    I am an Arabic AI assistant specialized in providing detailed and accurate answers across various fields.
+    You are an Arabic AI assistant specialized in providing detailed and accurate answers across various fields.
+    Your task is to deliver clear, concise, and relevant information. 
     """
 
     return [
@@ -45,15 +46,17 @@ def prompt(input_sample):
             "role": "user",
             "content": question_prompt,
         },
-        {
-            "role": "assistant",
-            "content": assistant_prompt,
-        },
+        # {
+        #     "role": "assistant",
+        #     "content": assistant_prompt,
+        # },
     ]
 
 
 def post_process(response):
     data = response["output"]
+    if "\n\n" in data:
+        data = data.split("\n\n")[0]
     response = json.loads(data)
     answer = response["answer"]
     return answer
