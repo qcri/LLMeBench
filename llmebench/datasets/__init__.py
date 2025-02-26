566229a1b910b6bf80bf23dd0c8c64b8588567c1
Basel Mousi <59998313+baselmousi@users.noreply.github.com>
2025-02-26 20:23:14 +0300

    Feat/dialect identification shami corpus (#207)
    * add temp mt file
    
    * added data loader for the Shami Corpus
    
    * exported the dataset in init file
    
    * added a chatgpt ZS asset for dialect identification on Shami Corpus
    
    * Added gpt4 zero-shot asset for dialect identification on Shami Corpus
    
    * Added zero shot asset for dialect identification - bloom
    
    * formatted code for shami corpus dialect identification

--- llmebench/datasets/__init__.py
+++ llmebench/datasets/__init__.py
@@ -54,6 +54,7 @@ from .SANADAlKhaleej import SANADAlKhaleejDataset
 from .SemEval17T1STS import SemEval17T1STSDataset
 from .SemEval17T2STS import SemEval17T2STSDataset
 from .SemEval23T3Propaganda import SemEval23T3PropagandaDataset
+from .ShamiCorpus import ShamiDataset
 from .Spam import SpamDataset
 from .SpokenNativQA import SpokenNativQADataset
 from .STSQ2Q import STSQ2QDataset
