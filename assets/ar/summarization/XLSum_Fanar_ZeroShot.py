c83f8a1f628faf2648c0f8d37a6c65414e285fe9
MohamedBayan <118048819+MohamedBayan@users.noreply.github.com>
2025-03-21 20:41:36 +0300

    Xlsum ar assets (#394)
    * Add propaganda assets
    
    * Fix errors
    
    * Adding xlsum assets

---
+++ assets/ar/summarization/XLSum_Fanar_ZeroShot.py
@@ -0,0 +1,48 @@
+import json
+import re
+
+from llmebench.datasets import XLSumDataset
+from llmebench.models import OpenAIModel
+from llmebench.tasks import NLGenerationTask
+
+
+def metadata():
+    return {
+        "author": "Arabic Language Technologies, QCRI, HBKU",
+        "model": "Fanar",
+        "description": "FANAR-C model",
+        "scores": {},
+    }
+
+
+def config():
+    return {
+        "dataset": XLSumDataset,
+        "task": NLGenerationTask,
+        "model": OpenAIModel,
+        "general_args": {"test_split": "test"},
+    }
+
+
+def prompt(input_sample):
+    # Define the question prompt
+    user_prompt = f"""
+    {input_sample['instruction']}
+
+    {input_sample['input']}
+    """
+
+    # Define the assistant prompt
+    system_prompt = """
+    You are an Arabic AI assistant specialized in providing detailed and accurate answers across various fields. Your task is to deliver clear, concise, and relevant information. 
+    """
+    return [
+        {"role": "user", "content": user_prompt},
+        {"role": "system", "content": system_prompt},
+    ]
+
+
+def post_process(response):
+    content = response["choices"][0]["message"]["content"].strip()
+
+    return content
